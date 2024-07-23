9f80093433c984d803859cc17de789e8f2f9ed13
PauloErnani <pauloernanibernardes@gmail.com>
2024-07-23 10:29:16 -0300

    Add primeiro desafio

--- desafio/src/app/app.routes.ts
+++ desafio/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Desafio1Component } from './desafio1/desafio1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'desafio1',
+    component: Desafio1Component
+  },
+];
